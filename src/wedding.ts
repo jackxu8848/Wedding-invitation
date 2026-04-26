ff8af7f352dd65b9217f11b9833fe0c379822a85
jackxu8848 <jackxu8848@gmail.com>
2026-04-25 22:37:03 -0400

    Create wedding.ts

---
+++ src/wedding.ts
@@ -0,0 +1,10 @@
+export const WEDDING = {
+  partner1: "Qiu Tong Xu",
+  partner2: "Qing Han",
+  date: "July 23, 2026",
+  time: "3:30 PM",
+  venue: "Spencer's at the Waterfront",
+  address: "1340 Lakeshore Rd., Burlington, ON L7S 1B1",
+  mapsUrl:
+    "https://www.google.com/maps/search/?api=1&query=Spencer%27s%20at%20the%20Waterfront%2C%201340%20Lakeshore%20Rd%2C%20Burlington%2C%20ON",
+} as const;
